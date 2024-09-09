d4921e66a5d87c12bab9ddc7aceb6eb359fb7865
Vikas24Chauhan <vikas24chauhan@gmail.com>
2024-09-09 14:23:58 +0530

    media query applied

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,32 +1,11 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
 import "./Header.css";
+import Logo from "../../images/logo.png";
 
 function Header() {
   return (
     <div className="header-container">
-      <ul>
-        {/* <li>
-          <NavLink to="/">About</NavLink>
-        </li>
-        <li>
-          <NavLink to="/skills">Skills</NavLink>
-        </li>
-        <li>
-          <NavLink to="/experience">Experience</NavLink>
-        </li>
-        <li>
-          <NavLink to="/projects">Projects</NavLink>
-        </li>
-        <li>
-          <NavLink to="/education">Education</NavLink>
-        </li> */}
-        <li>About</li>
-        <li>Skills</li>
-        <li>Experience</li>
-        <li>Projects</li>
-        <li>Education</li>
-      </ul>
+      <img src={Logo} alt="PortFolio" />
     </div>
   );
 }
